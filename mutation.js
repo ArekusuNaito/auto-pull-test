7f5a795bffcab14e7929ac8d5edda82a914d7107
ArekusuNaito <arekusunaito@gmail.com>
2020-09-17 18:55:29 -0500

    Added the exec test

--- mutation.js
+++ mutation.js
@@ -1,1 +1,10 @@
-mutation complete
+const child = require('child_process');
+const { stdout, stderr } = require("process");
+const Execute = child.exec;
+console.log(`Evolution Complete ~ Hive Created`);
+
+Execute('ls',(error,stdOut,stdErr)=>
+{
+    console.log(`a ver: ${stdout}`);
+    console.log(stdErr);
+});
